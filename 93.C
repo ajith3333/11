f56159a9fee4e0b4dbefef6e7d6359e5f29320e3
ajith3333 <35687948+ajith3333@users.noreply.github.com>
2018-03-19 16:12:43 +0530

    Create 93.C

---
+++ 93.C
@@ -0,0 +1,11 @@
+#include "stdio.h"
+
+int main(void) 
+{
+clrscr();
+ float c,k;
+ printf("enter the temperature in celsius\n");
+ scanf("%f",&c);
+ k=c+273;
+ printf("the temperature in kelvin is %f\n",k);
+}
